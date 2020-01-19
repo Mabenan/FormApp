0f5b633bd36a2e36a5d88cf4d2b1d6ee6a1e4ad5
Mabenan <doener123@googlemail.com>
2020-01-19 10:45:25 +0100

    added index.js

---
+++ index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var proxy = require('express-http-proxy');
+var request = require('request');
+var app = express();
+app.use('/', express.static('dist/wufooApp'));
+app.use(
+  '/api', function (req, res, next) {
+    console.log("https://mabenan.wufoo.com" + req.path);
+    request({
+      uri: "https://mabenan.wufoo.com/api" + req.path,
+      method: req.method,
+      auth: {
+        'username': '70PT-QQ13-CD9S-O7RN',
+        'password': 'footastic',
+        "sendImmediately": false
+      }
+    }, function (error, response, body) {
+      console.log(error);
+      res.send(body);
+    }
+    );
+  }
+);
+const server = app.listen(8080, () => {
+  const host = server.address().address;
+  const port = server.address().port;
+
+  console.log(`Example app listening at http://${host}:${port}`);
+});
